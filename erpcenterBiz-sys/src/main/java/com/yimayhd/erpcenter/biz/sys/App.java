24929a9b29310de693159a653c76b21d625ec2b9
zhangjian <zhangjian@yimayholiday.com>
2016-10-17 14:32:11 +0800

    增加系统管理facade\biz\dal

---
+++ erpcenterBiz-sys/src/main/java/com/yimayhd/erpcenter/biz/sys/App.java
@@ -0,0 +1,13 @@
+package com.yimayhd.erpcenter.biz.sys;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ erpcenterFacade-sys-client/src/main/java/com/yimayhd/erpcenter/facade/sys/client/App.java
@@ -0,0 +1,13 @@
+package com.yimayhd.erpcenter.facade.sys.client;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
