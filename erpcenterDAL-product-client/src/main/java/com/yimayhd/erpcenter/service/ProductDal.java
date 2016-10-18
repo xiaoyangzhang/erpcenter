e3f4fe2574aea63d7aeb7de1888adc42ace4417f
zhangxiaoyang <xiaoyangzhang1990@163.com>
2016-10-18 10:15:24 +0800

    修改类名，

--- erpcenterDAL-product-client/src/main/java/com/yimayhd/erpcenter/service/ProductDal.java
+++ erpcenterDAL-product-client/src/main/java/com/yimayhd/erpcenter/service/ProductDal.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import com.yihg.mybatis.utility.PageBean;
 import com.yimayhd.erpcenter.dal.product.po.ProductInfo;
+import com.yimayhd.erpcenter.dal.product.po.ProductRoute;
 import com.yimayhd.erpcenter.dal.product.vo.ProductInfoVo;
 
 
@@ -29,4 +30,24 @@ public interface ProductDal {
 	* @throws
 	 */
 	List<ProductInfo> selectProductListPage(PageBean pageBean,Map<String, Object> parameters);
+	/**
+	 * 分页查询产品列表
+	 * @param pageBean
+	 * @param bizId
+	 * @return
+	 */
+	PageBean<ProductInfo> findProductInfos(PageBean<ProductInfo> pageBean,Map parameters);
+	/**
+	 * 修改产品
+	 * @param productInfo
+	 * @return
+	 */
+	int updateProductInfo(ProductInfo productInfo);
+
+	/**
+	 *
+	 * @param productInfo
+	 * @return
+	 */
+	List<ProductRoute> findProductRouteByProductId(Integer productId);
 }
--- erpcenterDAL-product/src/main/java/com/yimayhd/erpcenter/dal/product/service/impl/ProductDalImpl.java
+++ erpcenterDAL-product/src/main/java/com/yimayhd/erpcenter/dal/product/service/impl/ProductDalImpl.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import com.yihg.mybatis.utility.PageBean;
 import com.yimayhd.erpcenter.dal.product.po.ProductInfo;
+import com.yimayhd.erpcenter.dal.product.po.ProductRoute;
 import com.yimayhd.erpcenter.dal.product.vo.ProductInfoVo;
 import com.yimayhd.erpcenter.service.ProductDal;
 
@@ -34,6 +35,50 @@ public class ProductDalImpl implements ProductDal{
 	@Override
 	public List<ProductInfo> selectProductListPage(PageBean pageBean,
 			Map<String, Object> parameters) {
+		return null;
+	}
+
+
+	/* (non-Javadoc)
+	 * <p>Title: findProductInfos</p> 
+	 * <p>Description: </p> 
+	 * @param pageBean
+	 * @param parameters
+	 * @return 
+	 * @see com.yimayhd.erpcenter.service.ProductDal#findProductInfos(com.yihg.mybatis.utility.PageBean, java.util.Map)
+	 */
+	@Override
+	public PageBean<ProductInfo> findProductInfos(
+			PageBean<ProductInfo> pageBean, Map parameters) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	/* (non-Javadoc)
+	 * <p>Title: updateProductInfo</p> 
+	 * <p>Description: </p> 
+	 * @param productInfo
+	 * @return 
+	 * @see com.yimayhd.erpcenter.service.ProductDal#updateProductInfo(com.yimayhd.erpcenter.dal.product.po.ProductInfo)
+	 */
+	@Override
+	public int updateProductInfo(ProductInfo productInfo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+	/* (non-Javadoc)
+	 * <p>Title: findProductRouteByProductId</p> 
+	 * <p>Description: </p> 
+	 * @param productId
+	 * @return 
+	 * @see com.yimayhd.erpcenter.service.ProductDal#findProductRouteByProductId(java.lang.Integer)
+	 */
+	@Override
+	public List<ProductRoute> findProductRouteByProductId(Integer productId) {
+		// TODO Auto-generated method stub
 		return null;
 	}
 
