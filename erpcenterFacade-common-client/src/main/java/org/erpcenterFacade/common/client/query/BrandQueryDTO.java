80348fdca72f6e8063d629a9c3bed845904b1358
xueshengdong <123456789>
2016-10-19 19:44:18 +0800

    产品上下架

--- erpcenterFacade-common-client/src/main/java/org/erpcenterFacade/common/client/query/BrandQueryDTO.java
+++ erpcenterFacade-common-client/src/main/java/org/erpcenterFacade/common/client/query/BrandQueryDTO.java
@@ -1,12 +1,14 @@
 package org.erpcenterFacade.common.client.query;
 
+import java.io.Serializable;
+
 /**
  * @ClassName: ${ClassName}
  * @Description: TODO(这里用一句话描述这个类的作用)
  * @Auther xueshengdong
  * @Date 2016/10/19 11:22
  */
-public class BrandQueryDTO {
+public class BrandQueryDTO implements Serializable {
     private static final long serialVersionUID = 7546504127191622861L;
     private Integer bizId;
 
