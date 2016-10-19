e3493dff0f1a4cc1555350b66c6836a9b2622199
zhangxiaoyang <xiaoyangzhang1990@163.com>
2016-10-19 11:06:30 +0800

    计调查询参数组织接口

--- erpcenterFacade-common/src/main/java/com/yimayhd/erpcenter/facade/common/service/impl/ProductFacadeCommonImpl.java
+++ erpcenterFacade-common/src/main/java/com/yimayhd/erpcenter/facade/common/service/impl/ProductFacadeCommonImpl.java
@@ -1,13 +1,18 @@
 package com.yimayhd.erpcenter.facade.common.service.impl;
 
-import com.yimayhd.erpcenter.biz.basic.service.DicBiz;
-import com.yimayhd.erpcenter.biz.sys.service.PlatformEmployeeBiz;
-import com.yimayhd.erpcenter.biz.sys.service.PlatformOrgBiz;
-import com.yimayhd.erpcenter.common.contants.BasicConstants;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.commons.lang3.StringUtils;
 import org.erpcenterFacade.common.client.query.LoadProductDepartmentDTO;
 import org.erpcenterFacade.common.client.result.LoadProductDepartmentResult;
 import org.erpcenterFacade.common.client.service.ProductFacadeCommon;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import com.yimayhd.erpcenter.biz.basic.service.DicBiz;
+import com.yimayhd.erpcenter.biz.sys.service.PlatformEmployeeBiz;
+import com.yimayhd.erpcenter.biz.sys.service.PlatformOrgBiz;
+import com.yimayhd.erpcenter.common.contants.BasicConstants;
 
 /**
  * @ClassName: ${ClassName}
@@ -38,4 +43,34 @@ public class ProductFacadeCommonImpl implements ProductFacadeCommon {
         loadProductDepartmentResult.setOrgUserJsonStr(platformEmployeeBiz.getComponentOrgUserTreeJsonStr(loadProductDepartmentDTO.getBizId()));
         return loadProductDepartmentResult;
     }
+
+	/* (non-Javadoc)
+	 * <p>Title: setSaleOperatorIds</p> 
+	 * <p>Description: </p> 
+	 * @param operatorIds
+	 * @param orgIds
+	 * @return 
+	 * @see org.erpcenterFacade.common.client.service.ProductFacadeCommon#setSaleOperatorIds(java.lang.String, java.lang.String)
+	 */
+	@Override
+	public String setSaleOperatorIds(String operatorIds, String orgIds,int bizId) {
+		String saleOperatorIds = "";
+		if (StringUtils.isBlank(operatorIds) && StringUtils.isNotBlank(orgIds)) {
+			Set<Integer> set = new HashSet<Integer>();
+			String[] orgIdArr = orgIds.split(",");
+			for (String orgIdStr : orgIdArr) {
+				set.add(Integer.valueOf(orgIdStr));
+			}
+			set = platformEmployeeBiz.getUserIdListByOrgIdList(
+					bizId, set);
+			String salesOperatorIds = "";
+			for (Integer usrId : set) {
+				salesOperatorIds += usrId + ",";
+			}
+			if (!salesOperatorIds.equals("")) {
+				saleOperatorIds = salesOperatorIds.substring(0,salesOperatorIds.length() - 1);
+			}
+		}
+		return saleOperatorIds;
+	}
 }
