ef087f16e0fbe7bf67340105b1f2803f1afb86b1
zhangjian <zhangjian@yimayholiday.com>
2016-10-20 10:36:42 +0800

    增加product topic

---
+++ erpcenterDAL-product-client/src/main/java/com/yimayhd/erpcenter/dal/product/topic/ProductTopic.java
@@ -0,0 +1,53 @@
+package com.yimayhd.erpcenter.dal.product.topic;
+
+
+public enum ProductTopic {
+
+    PRODUCT_MODIFY(ProductTopic.TOPIC,"PRODUCT_MODIFY","用户注销");
+	
+    public static final String TOPIC = "ERP_PRODUCT";
+    /**
+     * 主题
+     */
+    private String topic;
+
+    /**
+     * 标签
+     */
+    private String tags;
+
+    /**
+     * 描述
+     */
+    private String desc;
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public String getTags() {
+        return tags;
+    }
+
+    public void setTags(String tags) {
+        this.tags = tags;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    private ProductTopic(String topic, String tags, String desc) {
+        this.topic = topic;
+        this.tags = tags;
+        this.desc = desc;
+    }
+}
