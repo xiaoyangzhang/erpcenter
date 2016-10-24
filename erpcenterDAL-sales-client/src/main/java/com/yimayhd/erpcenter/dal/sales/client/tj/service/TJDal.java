8751aeedf7d28be9fab788efc190b1b9f31b504d
wangjun <wangjun@jiuxiulvxing.com>
2016-10-24 15:45:03 +0800

    1

--- erpcenterDAL-sales-client/src/main/java/com/yimayhd/erpcenter/dal/sales/client/tj/service/TJDal.java
+++ erpcenterDAL-sales-client/src/main/java/com/yimayhd/erpcenter/dal/sales/client/tj/service/TJDal.java
@@ -4,16 +4,14 @@ import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.ibatis.annotations.Param;
-
 import com.yihg.mybatis.utility.PageBean;
-import com.yihg.tj.po.TJGroupProfit;
-import com.yihg.tj.po.TJGroupQueryId;
-import com.yihg.tj.po.TJGroupShop;
-import com.yihg.tj.po.TJRecord;
-import com.yihg.tj.po.TJShopProject;
+import com.yimayhd.erpcenter.dal.sales.client.tj.po.TJGroupProfit;
+import com.yimayhd.erpcenter.dal.sales.client.tj.po.TJGroupQueryId;
+import com.yimayhd.erpcenter.dal.sales.client.tj.po.TJGroupShop;
+import com.yimayhd.erpcenter.dal.sales.client.tj.po.TJRecord;
+import com.yimayhd.erpcenter.dal.sales.client.tj.po.TJShopProject;
 
-public interface TJService {
+public interface TJDal {
 
 	PageBean  tjShopProject(PageBean pageBean,String start);
 	
