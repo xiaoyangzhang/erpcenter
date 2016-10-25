6dc52917e7e7ca5843a0e4c8be615d53ebb3a145
xueshengdong <123456789>
2016-10-25 14:42:06 +0800

    biz

--- erpcenterBiz-sales/src/main/java/com/yimayhd/erpcenter/biz/sales/service/impl/quality/QualityBizImpl.java
+++ erpcenterBiz-sales/src/main/java/com/yimayhd/erpcenter/biz/sales/service/impl/quality/QualityBizImpl.java
@@ -6,13 +6,15 @@ import javax.annotation.Resource;
 
 import com.yihg.mybatis.utility.PageBean;
 import com.yimayhd.erpcenter.biz.sales.client.service.quality.QualityBiz;
+import com.yimayhd.erpcenter.dal.sales.client.quality.service.QualityDal;
 import com.yimayhd.erpcenter.dal.sales.client.quality.vo.QualityTourGroupVo;
 import com.yimayhd.erpcenter.dal.sales.client.sales.po.GroupOrder;
 import com.yimayhd.erpcenter.dal.sales.client.sales.service.GroupOrderDal;
 import com.yimayhd.erpcenter.dal.sales.client.sales.service.TourGroupDal;
 
 public class QualityBizImpl implements QualityBiz {
-
+	@Resource
+	private QualityDal qualityDal;
 	@Resource
 	private TourGroupDal tourGroupDal;
 	@Resource
@@ -21,25 +23,7 @@ public class QualityBizImpl implements QualityBiz {
 	@Override
 	public PageBean getQualityTourGroupList(
 			PageBean<QualityTourGroupVo> pageBean) {
-		List<QualityTourGroupVo> list = tourGroupDal.selectQualityTourGroupListPage(pageBean);
-		pageBean.setResult(fillGroupInfo(list));		
-		return pageBean;
-	}
-	
-	private List<QualityTourGroupVo> fillGroupInfo(List<QualityTourGroupVo> list){
-		if(list!=null && list.size()>0){
-			for(QualityTourGroupVo vo : list){
-				if(vo.getGroupMode()>0){
-					List<GroupOrder> orderList = groupOrderDal.selectOrderByGroupId(vo.getGroupId());
-					if(orderList!=null && orderList.size()>0){
-						vo.setSupplierId(orderList.get(0).getSupplierId());
-						vo.setSupplierName(orderList.get(0).getSupplierName());
-						vo.setOrderId(orderList.get(0).getId());
-					}
-				}
-			}
-		}
-		return list;
+		return qualityDal.getQualityTourGroupList(pageBean);
 	}
 
 }
