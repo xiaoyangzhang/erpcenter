8bd9c8b4852a1c49f702b1836b18e2193b777a0d
wangjun <wangjun@jiuxiulvxing.com>
2016-10-25 16:36:28 +0800

    1

---
+++ erpcenterFacade-sales-client/src/main/java/com/yimayhd/erpcenter/facade/sales/BaseListPage.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package com.yimayhd.erpcenter.facade.sales;
+
+import java.io.Serializable;
+
+/**
+ * @ClassName: BaseListPage
+ * @Description: TODO(这里用一句话描述这个类的作用)
+ * @author zhangxiaoyang
+ * @date 2016年10月18日
+ */
+public class BaseListPage implements Serializable {
+
+	/**
+	* @Fields serialVersionUID : TODO(用一句话描述这个变量表示什么)
+	*/
+	private static final long serialVersionUID = -2556824914327988809L;
+
+	private int page = 1;
+	private int pageSize = 20;
+	public int getPage() {
+		return page;
+	}
+	public void setPage(int page) {
+		this.page = page;
+	}
+	public int getPageSize() {
+		return pageSize;
+	}
+	public void setPageSize(int pageSize) {
+		this.pageSize = pageSize;
+	}
+	
+	
+}
---
+++ erpcenterFacade-sales-client/src/main/java/com/yimayhd/erpcenter/facade/sales/service/BookingShopFacade.java
@@ -0,0 +1,13 @@
+package com.yimayhd.erpcenter.facade.sales.service;
+
+/**
+ * 
+* @ClassName: BookingShopFacade 
+* @Description: 
+* @author wangjun
+* @date 2016年10月25日 下午4:32:17 
+*
+ */
+public interface BookingShopFacade {
+	
+}
