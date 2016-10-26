acdf5653eda08e2a41d4eaf94eb1f21d2494295d
xueshengdong <123456789>
2016-10-26 12:42:38 +0800

    团队管理 Facade

---
+++ erpcenterFacade-sales-client/src/main/java/com/yimayhd/erpcenter/facade/sales/result/ToGroupListResult.java
@@ -0,0 +1,51 @@
+package com.yimayhd.erpcenter.facade.sales.result;
+
+import com.yimayhd.erpcenter.dal.basic.po.DicInfo;
+import com.yimayhd.erpcenter.dal.basic.po.RegionInfo;
+
+import java.util.List;
+
+/**
+ * @ClassName: ${ClassName}
+ * @Description: TODO(这里用一句话描述这个类的作用)
+ * @Auther xueshengdong
+ * @Date 2016/10/26 11:22
+ */
+public class ToGroupListResult extends ResultSupport {
+    private List<RegionInfo> allProvince;
+    private String orgJsonStr;
+    private String orgUserJsonStr;
+    private List<DicInfo> sourceTypeList;
+
+    public List<RegionInfo> getAllProvince() {
+        return allProvince;
+    }
+
+    public void setAllProvince(List<RegionInfo> allProvince) {
+        this.allProvince = allProvince;
+    }
+
+    public String getOrgJsonStr() {
+        return orgJsonStr;
+    }
+
+    public void setOrgJsonStr(String orgJsonStr) {
+        this.orgJsonStr = orgJsonStr;
+    }
+
+    public String getOrgUserJsonStr() {
+        return orgUserJsonStr;
+    }
+
+    public void setOrgUserJsonStr(String orgUserJsonStr) {
+        this.orgUserJsonStr = orgUserJsonStr;
+    }
+
+    public List<DicInfo> getSourceTypeList() {
+        return sourceTypeList;
+    }
+
+    public void setSourceTypeList(List<DicInfo> sourceTypeList) {
+        this.sourceTypeList = sourceTypeList;
+    }
+}
---
+++ erpcenterFacade-sales-client/src/main/java/com/yimayhd/erpcenter/facade/sales/service/TeamGroupFacade.java
@@ -0,0 +1,40 @@
+package com.yimayhd.erpcenter.facade.sales.service;
+
+import com.yimayhd.erpcenter.facade.sales.query.FindTourGroupByConditionDTO;
+import com.yimayhd.erpcenter.facade.sales.query.ToAddTeamGroupInfoDTO;
+import com.yimayhd.erpcenter.facade.sales.result.ContactManListResult;
+import com.yimayhd.erpcenter.facade.sales.result.FindTourGroupByConditionResult;
+import com.yimayhd.erpcenter.facade.sales.result.ToAddTeamGroupInfoResult;
+import com.yimayhd.erpcenter.facade.sales.result.ToGroupListResult;
+
+/**
+ * @ClassName: ${ClassName}
+ * @Description: TODO(这里用一句话描述这个类的作用)
+ * @Auther xueshengdong
+ * @Date 2016/10/25 17:57
+ */
+public interface TeamGroupFacade {
+
+
+
+    public ToGroupListResult toGroupList(int bizId);
+    /***
+     * 获取团队列表数据
+     * @param findTourGroupByConditionDTO
+     * @return
+     */
+    public FindTourGroupByConditionResult findTourGroupByConditionLoadModel(FindTourGroupByConditionDTO findTourGroupByConditionDTO);
+
+
+    public ToAddTeamGroupInfoResult toAddTeamGroupInfo(ToAddTeamGroupInfoDTO toAddTeamGroupInfoDTO);
+
+    public ToAddTeamGroupInfoResult getDataByGroupId(ToAddTeamGroupInfoDTO toAddTeamGroupInfoDTO);
+
+
+    public ContactManListResult contactManList(int curBizId, int id);
+
+
+
+
+
+}
