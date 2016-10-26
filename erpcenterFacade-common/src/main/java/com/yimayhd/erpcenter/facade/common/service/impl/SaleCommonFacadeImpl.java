6a7b2a38573fdda8da73df1daa300f15a4388544
zhangxiaoyang <xiaoyangzhang1990@163.com>
2016-10-26 17:13:16 +0800

    解决冲突

--- erpcenterFacade-common/src/main/java/com/yimayhd/erpcenter/facade/common/service/impl/SaleCommonFacadeImpl.java
+++ erpcenterFacade-common/src/main/java/com/yimayhd/erpcenter/facade/common/service/impl/SaleCommonFacadeImpl.java
@@ -1,36 +1,4 @@
-<<<<<<< 8a989fc60c5453369ca5ddcb7a3b0a2d11ffa98f
-/**
- * 
- */
-package com.yimayhd.erpcenter.facade.common.service.impl;
 
-import java.util.List;
-
-import org.erpcenterFacade.common.client.service.SaleCommonFacade;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import com.yimayhd.erpcenter.biz.basic.service.DicBiz;
-import com.yimayhd.erpcenter.dal.basic.constant.BasicConstants;
-import com.yimayhd.erpcenter.dal.basic.po.DicInfo;
-
-/**
- * @ClassName: SaleCommonFacadeImpl
- * @Description: TODO(这里用一句话描述这个类的作用)
- * @author zhangxiaoyang
- * @date 2016年10月25日
- */
-public class SaleCommonFacadeImpl implements SaleCommonFacade {
-
-	@Autowired
-	private DicBiz dicBiz;
-	@Override
-	public List<DicInfo> getDeliveryItemList(Integer bizId) {
-		List<DicInfo> typeList = dicBiz.getListByTypeCode(BasicConstants.XMFY_DJXM, bizId);
-		return typeList;
-	}
-
-}
-=======
 /**
  * 
  */
@@ -73,4 +41,3 @@ public class SaleCommonFacadeImpl implements SaleCommonFacade {
 	}
 
 }
->>>>>>> 添加结果类
