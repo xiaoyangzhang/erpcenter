e6e539a9ae3e2d3e3b6932cc282498cdca09583c
zhangxiaoyang <xiaoyangzhang1990@163.com>
2016-10-26 17:41:45 +0800

    解决冲突

--- erpcenterFacade-sales-client/src/main/java/com/yimayhd/erpcenter/facade/sales/service/BookingDeliveryFacade.java
+++ erpcenterFacade-sales-client/src/main/java/com/yimayhd/erpcenter/facade/sales/service/BookingDeliveryFacade.java
@@ -1,72 +1,4 @@
-<<<<<<< 8a989fc60c5453369ca5ddcb7a3b0a2d11ffa98f
-/**
- * 
- */
-package com.yimayhd.erpcenter.facade.sales.service;
 
-import java.util.Map;
-
-import com.yimayhd.erpcenter.dal.sys.po.PlatformEmployeePo;
-import com.yimayhd.erpcenter.facade.sales.result.BookingDeliveryResult;
-import com.yimayhd.erpcenter.facade.sales.result.ResultSupport;
-import com.yimayhd.erpcenter.facade.sales.result.WebResult;
-
-/**
- * @ClassName: BookingDeliveryFacade
- * @Description: TODO(这里用一句话描述这个类的作用)
- * @author zhangxiaoyang
- * @date 2016年10月25日
- */
-public interface BookingDeliveryFacade {
-	/**
-	 * 
-	* created by zhangxiaoyang
-	* @date 2016年10月25日
-	* @Description:地接社订单明细列表
-	* @param 
-	* @return List<BookingDelivery>
-	* @throws
-	 */
-	BookingDeliveryResult getDeliveryBookingDetailList(Integer groupId);
-	/**
-	 * 
-	* created by zhangxiaoyang
-	* @date 2016年10月25日
-	* @Description:预定安排地接社
-	* @param 
-	* @return List<BookingDelivery>
-	* @throws
-	 */
-	BookingDeliveryResult getDeliveryBookingArrangeList(Integer groupId);
-	/**
-	 * 新增/编辑地接订单
-	* created by zhangxiaoyang
-	* @date 2016年10月25日
-	* @Description:
-	* @param 
-	* @return BookingDeliveryResult
-	* @throws
-	 */
-	BookingDeliveryResult getDeliveryEditInfo(Integer groupId,Integer bid);
-	/**
-	 * 保存地接计调订单
-	* created by zhangxiaoyang
-	* @date 2016年10月25日
-	* @Description:
-	* @param 
-	* @return ResultSupport
-	* @throws
-	 */
-	WebResult<Map<String, Object>> saveDeliveryBooking(String bookingStr,PlatformEmployeePo userInfo);
-	
-	ResultSupport angencyConfirm(Integer id);
-	
-	ResultSupport angencyDelete(Integer id);
-}
-=======
-/**
- * 
- */
 package com.yimayhd.erpcenter.facade.sales.service;
 
 import java.util.Map;
@@ -139,4 +71,3 @@ public interface BookingDeliveryFacade {
 	 */
 	BookingDeliveryResult getBookingDeliveryInfo(Integer bookingId);
 }
->>>>>>> 添加结果类
