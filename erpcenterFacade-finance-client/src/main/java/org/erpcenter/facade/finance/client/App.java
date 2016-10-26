3ea43865fd1453d7dcc29b743850eae37972ee0d
zhangjian <zhangjian@yimayholiday.com>
2016-10-26 20:45:15 +0800

    新加tj-facade,finance-facade,shoppingcart-facade,ticket-facade

---
+++ erpcenterFacade-finance-client/src/main/java/org/erpcenter/facade/finance/client/App.java
@@ -0,0 +1,13 @@
+package org.erpcenter.facade.finance.client;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ erpcenterFacade-finance/src/main/java/com/yimayhd/erpcenter/facade/finance/App.java
@@ -0,0 +1,13 @@
+package com.yimayhd.erpcenter.facade.finance;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
