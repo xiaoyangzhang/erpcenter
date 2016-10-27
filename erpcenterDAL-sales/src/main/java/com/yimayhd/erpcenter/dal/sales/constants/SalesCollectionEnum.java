881829dee7fc40e0d86057595f358fad188bb199
zhangjian <zhangjian@yimayholiday.com>
2016-10-27 11:47:02 +0800

    增加销售topic

---
+++ erpcenterDAL-sales/src/main/java/com/yimayhd/erpcenter/dal/sales/constants/SalesCollectionEnum.java
@@ -0,0 +1,61 @@
+package com.yimayhd.erpcenter.dal.sales.constants;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * 
+ 
+ *
+ */
+public enum SalesCollectionEnum {
+
+    PRODUCT_STATE("产品状态", 1, "productManager"),PRODUCT_STOCK("产品状态", 2, "productStock")
+    ;
+    
+    private String name;
+    private int id;
+    private String collection;
+
+    SalesCollectionEnum(String name, int id, String collection) {
+        this.name = name;
+        this.id = id;
+        this.collection =  collection;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCollection() {
+        return collection;
+    }
+
+    
+    public static SalesCollectionEnum getClientByCollection(String collection) {
+        if(StringUtils.isBlank(collection)){
+            return null;
+        }
+        for (SalesCollectionEnum solrdumpClient : SalesCollectionEnum.values()) {
+            if (solrdumpClient.getCollection().equals(collection)) {
+                return solrdumpClient;
+            }
+        }
+        return null;
+    }
+    
+    
+    public static SalesCollectionEnum getClientById(int id) {
+        for (SalesCollectionEnum solrdumpClient : SalesCollectionEnum.values()) {
+            if (solrdumpClient.getId() == id) {
+                return solrdumpClient;
+            }
+        }
+        return null;
+    }
+
+
+}
