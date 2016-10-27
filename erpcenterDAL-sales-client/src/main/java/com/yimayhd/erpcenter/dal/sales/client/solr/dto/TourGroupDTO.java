38cda4d5b6bf60cd972828fa2d9e504edfeb5ddd
zhangjian <zhangjian@yimayholiday.com>
2016-10-27 14:39:37 +0800

    增加tourgroupdto

--- erpcenterDAL-sales-client/src/main/java/com/yimayhd/erpcenter/dal/sales/client/solr/dto/TourGroupDTO.java
+++ erpcenterDAL-sales-client/src/main/java/com/yimayhd/erpcenter/dal/sales/client/solr/dto/TourGroupDTO.java
@@ -1,5 +1,46 @@
 package com.yimayhd.erpcenter.dal.sales.client.solr.dto;
 
-public class TourGroupDTO {
+import java.io.Serializable;
+
+public class TourGroupDTO implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3898684074431147626L;
+	
+	/**
+	 * 团id
+	 */
+	private Integer tgGroupId;
+	
+	/**
+	 * 团编号
+	 */
+	private String  tgGroupCode;
+	
+	/**
+	 * 0时为散客团，>0时为定制团(字典为团类别) -1一地散 -2初始团
+	 */
+    private Integer tgGroupMode;
+    
+    /**
+     * 供应商名称
+     */
+    private String gOsupplierName;
+    
+    
+    
+    
+	tg.date_start,
+	tg.product_brand_name,
+	tg.product_name,
+	tg.total_adult,
+	tg.total_child,
+	tg.total_guide,
+	tg.operator_name,
+	tg.group_state,
+	tg.create_time,
+	tg.operator_id
 
 }
