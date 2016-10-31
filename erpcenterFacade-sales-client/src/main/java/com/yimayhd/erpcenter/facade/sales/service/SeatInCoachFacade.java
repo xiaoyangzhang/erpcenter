8e19457bb9830d1270788a2dde0de70301c4bb51
xueshengdong <123456789>
2016-10-31 16:16:28 +0800

    索赔擦旅馆肉皮

---
+++ erpcenterFacade-sales-client/src/main/java/com/yimayhd/erpcenter/facade/sales/service/SeatInCoachFacade.java
@@ -0,0 +1,10 @@
+package com.yimayhd.erpcenter.facade.sales.service;
+
+/**
+ * @ClassName: ${ClassName}
+ * @Description: TODO(这里用一句话描述这个类的作用)
+ * @Auther xueshengdong
+ * @Date 2016/10/31 16:15
+ */
+public interface SeatInCoachFacade {
+}
