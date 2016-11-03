171495d0fbfcdaa1d58d6d4b031878984086ec8b
liyong <liyong1@jiuxiulvxing.com>
2016-11-03 11:02:22 +0800

    供应商facade

--- erpcenterFacade-supplier/src/main/java/com/yimayhd/erpcenter/facade/supplier/service/impl/ConsultFacadeImpl.java
+++ erpcenterFacade-supplier/src/main/java/com/yimayhd/erpcenter/facade/supplier/service/impl/ConsultFacadeImpl.java
@@ -2,16 +2,9 @@ package com.yimayhd.erpcenter.facade.supplier.service.impl;
 
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.util.WebUtils;
 
 import com.yihg.mybatis.utility.PageBean;
 import com.yimayhd.erpcenter.biz.basic.service.DicBiz;
