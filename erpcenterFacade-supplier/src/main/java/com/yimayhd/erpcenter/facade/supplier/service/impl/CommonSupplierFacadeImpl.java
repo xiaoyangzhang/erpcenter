6db564e5f45556db4619f33f6d76683080080d11
liyong <liyong1@jiuxiulvxing.com>
2016-11-03 14:48:22 +0800

    添加commonFacade

---
+++ erpcenterFacade-supplier/src/main/java/com/yimayhd/erpcenter/facade/supplier/service/impl/CommonSupplierFacadeImpl.java
@@ -0,0 +1,28 @@
+package com.yimayhd.erpcenter.facade.supplier.service.impl;
+
+import java.util.List;
+import java.util.Map;
+
+import com.yihg.mybatis.utility.PageBean;
+import com.yimayhd.erpcenter.facade.supplier.service.CommonSupplierFacade;
+import com.yimayhd.erpresource.biz.service.CommonSupplierBiz;
+
+public class CommonSupplierFacadeImpl implements CommonSupplierFacade {
+
+	private CommonSupplierBiz commonSupplierBiz;
+	@Override
+	public PageBean queryListPage(String sqlId, PageBean pageBean) {
+		return commonSupplierBiz.queryListPage(sqlId, pageBean);
+	}
+
+	@Override
+	public List queryList(String sqlId, Map param) {
+		return commonSupplierBiz.queryList(sqlId, param);
+	}
+
+	@Override
+	public Map queryOne(String sqlId, Map param) {
+		return commonSupplierBiz.queryOne(sqlId, param);
+	}
+
+}
