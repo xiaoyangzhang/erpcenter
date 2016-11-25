09f4264a11826b29b492f80df7582773ec6531ff
gengxx <gengxiaoxuan@jiuxiulvxing.com>
2016-11-25 16:01:33 +0800

    taobao相关代码更新

---
+++ erpcenterBiz-sales/src/main/java/com/yimayhd/erpcenter/biz/sales/service/impl/util/HttpUtil.java
@@ -0,0 +1,73 @@
+package com.yimayhd.erpcenter.biz.sales.service.impl.util;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class HttpUtil {
+
+    
+	private static final Logger logger = LoggerFactory.getLogger(HttpUtil.class);
+    
+    /**
+     * 远程访问
+     *
+     * @param url 地址
+     * @param map 参数
+     * @return 远程数据
+     */
+    public static String doPost(String url, Map<String, String> map) {
+        CloseableHttpClient closeableHttpClient = HttpClients.createDefault();
+        
+        HttpPost httpPost = new HttpPost(url);
+        
+        List<NameValuePair> nameValuePairList = new ArrayList<NameValuePair>();
+        
+        for (Map.Entry<String, String> mapEntry : map.entrySet()) {
+            nameValuePairList.add(new BasicNameValuePair(mapEntry.getKey(), mapEntry.getValue()));
+        }
+        
+        CloseableHttpResponse closeableHttpResponse = null;
+        String response = "";
+        try {
+            httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairList));
+            
+            closeableHttpResponse = closeableHttpClient.execute(httpPost);
+            
+            HttpEntity httpEntity = closeableHttpResponse.getEntity();
+            
+            response = EntityUtils.toString(httpEntity);
+            
+        } catch (UnsupportedEncodingException e) {
+            logger.error("UnsupportedEncodingException:" + e.getMessage());
+        } catch (IOException e) {
+            logger.error("IOException:" + e.getMessage());
+        } finally {
+            try {
+                closeableHttpResponse.close();
+            } catch (IOException e) {
+                logger.error("IOException:" + e.getMessage());
+            }
+        }
+        
+        httpPost.abort();
+        
+        return response;
+    }
+
+}
