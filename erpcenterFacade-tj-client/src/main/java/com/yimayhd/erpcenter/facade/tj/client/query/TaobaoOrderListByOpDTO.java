32e2c448cf9d2a57edeed2a471df51ce6ec064d5
gengxx <gengxiaoxuan@jiuxiulvxing.com>
2016-11-29 14:45:53 +0800

    taobao更新

--- erpcenterFacade-tj-client/src/main/java/com/yimayhd/erpcenter/facade/tj/client/query/TaobaoOrderListByOpDTO.java
+++ erpcenterFacade-tj-client/src/main/java/com/yimayhd/erpcenter/facade/tj/client/query/TaobaoOrderListByOpDTO.java
@@ -1,10 +1,14 @@
 package com.yimayhd.erpcenter.facade.tj.client.query;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Set;
 
+import com.yihg.mybatis.utility.PageBean;
 import com.yimayhd.erpcenter.dal.basic.po.DicInfo;
 import com.yimayhd.erpcenter.dal.basic.po.RegionInfo;
+import com.yimayhd.erpcenter.dal.sales.client.sales.po.GroupOrder;
 
 public class TaobaoOrderListByOpDTO implements Serializable{
 
@@ -16,6 +20,86 @@ public class TaobaoOrderListByOpDTO implements Serializable{
 	private List<DicInfo> sourceTypeList;
 	private String orgJsonStr;
 	private String orgUserJsonStr;
+	
+	private GroupOrder groupOrder;
+	private Set<Integer> dataUserIdSets;
+	/**
+	 * @return the dataUserIdSets
+	 */
+	public Set<Integer> getDataUserIdSets() {
+		return dataUserIdSets;
+	}
+	/**
+	 * @param dataUserIdSets the dataUserIdSets to set
+	 */
+	public void setDataUserIdSets(Set<Integer> dataUserIdSets) {
+		this.dataUserIdSets = dataUserIdSets;
+	}
+	/**
+	 * @return the pageTotalAudit
+	 */
+	public Integer getPageTotalAudit() {
+		return pageTotalAudit;
+	}
+	/**
+	 * @param pageTotalAudit the pageTotalAudit to set
+	 */
+	public void setPageTotalAudit(Integer pageTotalAudit) {
+		this.pageTotalAudit = pageTotalAudit;
+	}
+	/**
+	 * @return the pageTotalChild
+	 */
+	public Integer getPageTotalChild() {
+		return pageTotalChild;
+	}
+	/**
+	 * @param pageTotalChild the pageTotalChild to set
+	 */
+	public void setPageTotalChild(Integer pageTotalChild) {
+		this.pageTotalChild = pageTotalChild;
+	}
+	/**
+	 * @return the pageTotalGuide
+	 */
+	public Integer getPageTotalGuide() {
+		return pageTotalGuide;
+	}
+	/**
+	 * @param pageTotalGuide the pageTotalGuide to set
+	 */
+	public void setPageTotalGuide(Integer pageTotalGuide) {
+		this.pageTotalGuide = pageTotalGuide;
+	}
+	/**
+	 * @return the pageTotal
+	 */
+	public BigDecimal getPageTotal() {
+		return pageTotal;
+	}
+	/**
+	 * @param pageTotal the pageTotal to set
+	 */
+	public void setPageTotal(BigDecimal pageTotal) {
+		this.pageTotal = pageTotal;
+	}
+	/**
+	 * @return the page
+	 */
+	public PageBean<GroupOrder> getPage() {
+		return page;
+	}
+	/**
+	 * @param page the page to set
+	 */
+	public void setPage(PageBean<GroupOrder> page) {
+		this.page = page;
+	}
+	private Integer pageTotalAudit;
+	private Integer pageTotalChild;
+	private Integer pageTotalGuide;
+	private BigDecimal pageTotal;
+	private PageBean<GroupOrder> page;
 	/**
 	 * @return the bizId
 	 */
@@ -99,6 +183,18 @@ public class TaobaoOrderListByOpDTO implements Serializable{
 	 */
 	public void setOrgUserJsonStr(String orgUserJsonStr) {
 		this.orgUserJsonStr = orgUserJsonStr;
+	}
+	/**
+	 * @return the groupOrder
+	 */
+	public GroupOrder getGroupOrder() {
+		return groupOrder;
+	}
+	/**
+	 * @param groupOrder the groupOrder to set
+	 */
+	public void setGroupOrder(GroupOrder groupOrder) {
+		this.groupOrder = groupOrder;
 	}
 	
 	
