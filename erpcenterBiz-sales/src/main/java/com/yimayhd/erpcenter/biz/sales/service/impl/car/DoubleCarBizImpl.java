9a20eded1ed9be37bd8b9f9a24a608a541e1bde9
wangjun <wangjun@jiuxiulvxing.com>
2016-12-12 10:28:49 +0800

    1

--- erpcenterBiz-sales/src/main/java/com/yimayhd/erpcenter/biz/sales/service/impl/car/DoubleCarBizImpl.java
+++ erpcenterBiz-sales/src/main/java/com/yimayhd/erpcenter/biz/sales/service/impl/car/DoubleCarBizImpl.java
@@ -1,7 +1,20 @@
 package com.yimayhd.erpcenter.biz.sales.service.impl.car;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.yimayhd.erpcenter.biz.sales.client.service.car.DoubleCarBiz;
+import com.yimayhd.erpcenter.dal.sales.client.car.po.TransPort;
+import com.yimayhd.erpcenter.dal.sales.client.car.service.DoubleCarDal;
 
 public class DoubleCarBizImpl implements DoubleCarBiz{
+	@Autowired
+	private DoubleCarDal doubleCarDal;
+
+	@Override
+	public List<TransPort> selectTransportByOrderId(int orderId) {
+		return doubleCarDal.selectTransportByOrderId(orderId);
+	}
 
 }
