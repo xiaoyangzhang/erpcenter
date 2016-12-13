798a5b183e730ae55d26376d8ea980a2a6d3c428
zhangxy <xiaoyangzhang1990@163.com>
2016-12-13 18:32:39 +0800

    订单接口

--- erpcenter-web/src/test/java/com/yimayhd/test/CarCarInterfaceTest.java
+++ erpcenter-web/src/test/java/com/yimayhd/test/CarCarInterfaceTest.java
@@ -2,9 +2,12 @@ package com.yimayhd.test;
 
 import com.alibaba.fastjson.JSON;
 import com.yihg.mybatis.utility.PageBean;
+import com.yimayhd.erpcenter.dal.sales.client.sales.po.GroupOrderForCarCar;
 import com.yimayhd.erpcenter.dal.sales.client.sales.po.TourGroupForCarCar;
 import com.yimayhd.erpcenter.dal.sales.client.sales.query.GroupInfoQueryForCarCar;
+import com.yimayhd.erpcenter.dal.sales.client.sales.query.GroupOrderQueryForCarCar;
 import com.yimayhd.erpcenter.facade.sales.result.WebResult;
+import com.yimayhd.erpcenter.facade.sales.service.GroupOrderFacade;
 import com.yimayhd.erpcenter.facade.sales.service.TourGroupFacade;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,7 +16,9 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.Calendar;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Created by DELL on 2016/12/13.
@@ -23,11 +28,13 @@ import java.util.List;
 public class CarCarInterfaceTest {
     @Autowired
     private TourGroupFacade tourGroupFacade;
-
+    @Autowired
+    private GroupOrderFacade groupOrderFacade;
 
     @Test
-    public void testTourGroupForCarCar() {
-        selectTourGroupsForCarCar();
+    public void testInterface() {
+//        selectTourGroupsForCarCar();
+        selectGroupOrdersForCarCar();
     }
 
     private void selectTourGroupsForCarCar() {
@@ -45,6 +52,19 @@ public class CarCarInterfaceTest {
         WebResult<List<TourGroupForCarCar>> result =
                 tourGroupFacade.selectGroupInfoWithArrangedTransAndGuideForCarCar(pageBean);
         System.out.println("--------------------"+ JSON.toJSONString(result));
+    }
+
+    private void selectGroupOrdersForCarCar() {
+        PageBean<GroupOrderQueryForCarCar> pageBean  = new PageBean<GroupOrderQueryForCarCar>();
+        pageBean.setPage(1);
+        pageBean.setPageSize(50);
+        GroupOrderQueryForCarCar car = new GroupOrderQueryForCarCar();
+        Set<Integer> set = new HashSet<Integer>();
+        set.add(14185);
+        set.add(14186);
+        car.setGroupIdSet(set);
+        pageBean.setParameter(car);
+        WebResult<List<GroupOrderForCarCar>> result = groupOrderFacade.selectGroupOrdersInOneGroupForCarCar(pageBean);
     }
 
 }
--- erpcenterDAL-sales-client/src/main/java/com/yimayhd/erpcenter/dal/sales/client/sales/po/GroupOrderForCarCar.java
+++ erpcenterDAL-sales-client/src/main/java/com/yimayhd/erpcenter/dal/sales/client/sales/po/GroupOrderForCarCar.java
@@ -1,6 +1,7 @@
 package com.yimayhd.erpcenter.dal.sales.client.sales.po;
 
 import java.io.Serializable;
+import java.util.List;
 
 /**
  * Created by DELL on 2016/12/9.
@@ -8,16 +9,16 @@ import java.io.Serializable;
 public class GroupOrderForCarCar implements Serializable {
     private static final long serialVersionUID = -707282179483459490L;
 
-    private GroupOrder groupOrder;
+    private List<GroupOrder> groupOrderList;
     private String groupCode;
     private String guestReresentativeName;
 
-    public GroupOrder getGroupOrder() {
-        return groupOrder;
+    public List<GroupOrder> getGroupOrderList() {
+        return groupOrderList;
     }
 
-    public void setGroupOrder(GroupOrder groupOrder) {
-        this.groupOrder = groupOrder;
+    public void setGroupOrderList(List<GroupOrder> groupOrderList) {
+        this.groupOrderList = groupOrderList;
     }
 
     public String getGroupCode() {
