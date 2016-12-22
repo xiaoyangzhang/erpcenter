804f11446cf2c82b4aa0f7e71418599a2f085bd8
zhangxy <xiaoyangzhang1990@163.com>
2016-12-22 17:38:46 +0800

    删除common依赖

--- erpcenterFacade-sales-client/src/main/java/com/yimayhd/erpcenter/facade/sales/query/FitUpdateTourGroupDTO.java
+++ erpcenterFacade-sales-client/src/main/java/com/yimayhd/erpcenter/facade/sales/query/FitUpdateTourGroupDTO.java
@@ -2,7 +2,9 @@ package com.yimayhd.erpcenter.facade.sales.query;
 
 import com.yimayhd.erpcenter.dal.sales.client.sales.po.TourGroup;
 
-public class FitUpdateTourGroupDTO {
+import java.io.Serializable;
+
+public class FitUpdateTourGroupDTO implements Serializable{
 	
 	private TourGroup tourGroup;
 
