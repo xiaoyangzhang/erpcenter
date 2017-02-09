1e89d6ceda3c2e7bead104100025cf6e1e9d99ab
zhangxy <xiaoyangzhang1990@163.com>
2017-02-09 09:44:40 +0800

    添加field

--- erpcenterDAL-sales-client/src/main/java/com/yimayhd/erpcenter/dal/sales/client/sales/query/GroupInfoQueryForCarCar.java
+++ erpcenterDAL-sales-client/src/main/java/com/yimayhd/erpcenter/dal/sales/client/sales/query/GroupInfoQueryForCarCar.java
@@ -11,6 +11,15 @@ public class GroupInfoQueryForCarCar implements Serializable {
     private long startTime;
     private long endTime;
     private String supplierName;
+    private String bizIds;
+
+    public String getBizIds() {
+        return bizIds;
+    }
+
+    public void setBizIds(String bizIds) {
+        this.bizIds = bizIds;
+    }
 
     public long getStartTime() {
         return startTime;
